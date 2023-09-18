6d55ea1442fa318a3c28dcdfb49716a7f0c656d9
Matej Bašić <matej.basic@outlook.com>
2023-09-18 12:19:38 +0200

    Added tailwind countdown with daisyUI and updated target date

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,24 @@ export default function Home() {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    const target = new Date("09/28/2023 23:59:59");
+    const target = new Date("11/16/2023 23:59:59");
 
     const interval = setInterval(() => {
       const now = new Date();
       const difference = target.getTime() - now.getTime();
 
-      const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
       setDays(days);
 
       const hours = Math.floor(
-        (difference / (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
       setHours(hours);
 
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
       setMinutes(minutes);
 
-      const seconds = Math.floor((difference % (1000 * 60)) / 1000)
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
       setSeconds(seconds);
 
       if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
@@ -41,38 +42,39 @@ export default function Home() {
     return () => clearInterval(interval);
   }, [])
 
-  if (isLoading) return <h1>Spremi se</h1>
+  if (isLoading) return <div className="container flex break-keep text-9xl">SPREMI SE</div>
   return (
   <div className="container">
     {obranaTime ? (
-      <h1>Spremi novce za sljedecu godinu</h1>
+      <div className="container flex break-keep text-9xl">PRIPREMI LOVU ZA SLJEDECU GODINU</div>
     ) : (
       <>
-
-      <h2>Diplomski countdown</h2>
-      <div className="timer-wrapper">
-        <div className="timer-inner">
-          <div className="timer-segment">
-            <span className="time">{days}</span>
-            <span className="label">Days</span>
-          </div>
-          <span className="divider">:</span>
-          <div className="timer-segment">
-            <span className="time">{hours}</span>
-            <span className="label">Hours</span>
-          </div>
-          <span className="divider">:</span>
-          <div className="timer-segment">
-            <span className="time">{minutes}</span>
-            <span className="label">Minutes</span>
-          </div>
-          <span className="divider">:</span>
-          <div className="timer-segment">
-            <span className="time">{seconds}</span>
-            <span className="label">Seconds</span>
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+          <div className="flex flex-col text-3xl">
+            <span className="countdown font-mono text-9xl">
+              <span style={{"--value":days}}></span>
+            </span>
+            days
+          </div> 
+          <div className="flex flex-col text-3xl">
+            <span className="countdown font-mono text-9xl">
+              <span style={{"--value":hours}}></span>
+            </span>
+            hours
+          </div> 
+          <div className="flex flex-col text-3xl">
+            <span className="countdown font-mono text-9xl">
+              <span style={{"--value":minutes}}></span>
+            </span>
+            min
+          </div> 
+          <div className="flex flex-col text-3xl">
+            <span className="countdown font-mono text-9xl">
+              <span style={{"--value":seconds}}></span>
+            </span>
+            sec
           </div>
         </div>
-      </div>
       </>
     )}
   </div>
