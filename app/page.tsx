34ecbfbd0b842d73fb1b74de57fa41b3aa78fd9e
visiot-dev <87547742+TarikDervisevic@users.noreply.github.com>
2023-09-18 20:32:25 +0200

    Added mobile responsivness
    -Added breakpoints with variable font size
    -Switched to rows instead of columns on xs breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -42,44 +42,45 @@ export default function Home() {
     return () => clearInterval(interval);
   }, [])
 
-  if (isLoading) return <div className="container flex break-keep text-9xl">SPREMI SE</div>
+  const responsiveTextLarge = "3xl:text-12xl 2xl:text-10xl xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-4xl"
+  const responsiveTextSmall = "3xl:text-5xl 2xl:text-4xl xl:text-3xl md:text-2xl sm:text-xl"
+
+  if (isLoading) return <div className={"flex break-keep h-screen w-screen justify-center items-center align-middle" + responsiveTextLarge}>SPREMI SE</div>
   return (
-  <div className="container">
+  <div className="w-full h-screen flex items-center justify-center">
     {obranaTime ? (
-      <div className="container flex break-keep text-9xl">PRIPREMI LOVU ZA SLJEDECU GODINU</div>
+      <div className={"flex break-keep text-center align-middle" + responsiveTextLarge}>PRIPREMI LOVU ZA SLJEDECU GODINU</div>
     ) : (
-      <>
-        <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
-          <div className="flex flex-col text-3xl">
-            <span className="countdown font-mono text-9xl">
-              {/* @ts-ignore */}
-              <span style={{"--value":days}}></span>
-            </span>
-            days
-          </div> 
-          <div className="flex flex-col text-3xl">
-            <span className="countdown font-mono text-9xl">
-              {/* @ts-ignore */}
-              <span style={{"--value":hours}}></span>
-            </span>
-            hours
-          </div> 
-          <div className="flex flex-col text-3xl">
-            <span className="countdown font-mono text-9xl">
-              {/* @ts-ignore */}
-              <span style={{"--value":minutes}}></span>
-            </span>
-            min
-          </div> 
-          <div className="flex flex-col text-3xl">
-            <span className="countdown font-mono text-9xl">
-              {/* @ts-ignore */}
-              <span style={{"--value":seconds}}></span>
-            </span>
-            sec
-          </div>
+      <div className="grid sm:grid-flow-col grid-flow-row gap-5 text-center sm:auto-cols-max auto-rows-auto">
+        <div className={"flex flex-col items-center" + responsiveTextSmall}>
+          <span className={"countdown font-mono" + responsiveTextLarge}>
+            {/* @ts-ignore */}
+            <span style={{"--value":days}}></span>
+          </span>
+          days
+        </div> 
+        <div className={"flex flex-col items-center" + responsiveTextSmall}>
+          <span className={"countdown font-mono" + responsiveTextLarge}>
+            {/* @ts-ignore */}
+            <span style={{"--value":hours}}></span>
+          </span>
+          hours
+        </div> 
+        <div className={"flex flex-col items-center" + responsiveTextSmall}>
+          <span className={"countdown font-mono" + responsiveTextLarge}>
+            {/* @ts-ignore */}
+            <span style={{"--value":minutes}}></span>
+          </span>
+          min
+        </div> 
+        <div className={"flex flex-col items-center" + responsiveTextSmall}>
+          <span className={"countdown font-mono" + responsiveTextLarge}>
+            {/* @ts-ignore */}
+            <span style={{"--value":seconds}}></span>
+          </span>
+          sec
         </div>
-      </>
+      </div>
     )}
   </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      'xl': '1rem',
+      '2xl': '1.5rem',
+      '3xl': '2rem',
+      '4xl': '3.25rem',
+      '5xl': '4rem',
+      '6xl': '5rem',
+      '7xl': '6.5rem',
+      '8xl': '8rem',
+      '9xl': '10rem',
+      '10xl': '12rem',
+      '11xl': '14rem',
+      '12xl': '16rem',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
